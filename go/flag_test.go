3e10dbf2e6236ead8a2fb626c1be30133903455f
Jacob Patterson <jacob.patterson@gmail.com>
2020-05-22 15:41:21 -0700

    adding more mainFlag tests

--- go/flag_test.go
+++ go/flag_test.go
@@ -17,7 +17,14 @@ func TestNewMainFlags(t *testing.T) {
 			want:   mainFlags{applicationName: "name2"},
 		},
 		{
+			osArgs: []string{"", "port=8001"},
+		},
+		{
 			osArgs: []string{"", "-port=8001"},
+			want:   mainFlags{serverPort: "8001"},
+		},
+		{
+			osArgs: []string{"", "--port=8001"},
 			want:   mainFlags{serverPort: "8001"},
 		},
 		{
@@ -38,8 +45,40 @@ func TestNewMainFlags(t *testing.T) {
 			want:    mainFlags{debugGame: true},
 		},
 		{
-
 			// 	osArgs: []string{"", "-h"}, // should print usage to console
+		},
+		{ // all command line
+			osArgs: []string{
+				"",
+				"-app-name=1",
+				"-port=2",
+				"-data-source=3",
+				"-words-file=4",
+				"-debug-game",
+			},
+			want: mainFlags{
+				applicationName: "1",
+				serverPort:      "2",
+				databaseURL:     "3",
+				wordsFile:       "4",
+				debugGame:       true,
+			},
+		},
+		{ // all environment variables
+			envVars: map[string]string{
+				"APPLICATION_NAME":    "1",
+				"PORT":                "2",
+				"DATABASE_URL":        "3",
+				"WORDS_FILE":          "4",
+				"DEBUG_GAME_MESSAGES": "",
+			},
+			want: mainFlags{
+				applicationName: "1",
+				serverPort:      "2",
+				databaseURL:     "3",
+				wordsFile:       "4",
+				debugGame:       true,
+			},
 		},
 	}
 	for i, test := range newMainFlagsTests {
