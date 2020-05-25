e2d221d7826acda8bac54c2e379e3bd741cda579
Jacob Patterson <jacob.patterson@gmail.com>
2020-05-25 15:11:03 -0700

    adding info about expected goroutines to monitor

--- go/server/monitor.go
+++ go/server/monitor.go
@@ -22,6 +22,20 @@ func (s Server) handleMonitor(w http.ResponseWriter, r *http.Request) error {
 
 	write()
 
+	goroutineExpectations := `Eight (8) goroutines are expected on an idling server:
+* a goroutine listening for interrut/termination signals so the server can stop gracefully
+* a goroutine to run the server
+* a goroutine to listen for new server requests
+* a goroutine to run the main procedure
+* a goroutine to keep open new sql database connections
+* a goroutine to reset existing sql database connections
+* a goroutine to run the lobby
+* a goroutine to write profiling information about goroutines
+
+Each player in the lobby should have two (2) goroutines to read and write websocket messages.
+Each game in the lobby runs on a single (1) goroutine.
+`
+	write(goroutineExpectations)
 	write("--- goroutine stack traces ---")
 	goroutineProfiles := pprof.Lookup("goroutine")
 	goroutineProfiles.WriteTo(w, 1)
