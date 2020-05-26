fcdad1f3cf33fccc6a42ca68a4f89699781b9eaa
Jacob Patterson <jacob.patterson@gmail.com>
2020-05-26 16:28:22 -0700

    adding gameWarning test

---
+++ go/game/controller/error_test.go
@@ -0,0 +1,12 @@
+package controller
+
+import "testing"
+
+func TestGameWarningError(t *testing.T) {
+	want := "x"
+	err := gameWarning(want)
+	got := err.Error()
+	if want != got {
+		t.Errorf("wanted gameWarning error string to be '%v', but was '%v'", want, got)
+	}
+}
