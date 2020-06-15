0559c4890fc261fe7e78ad07628637fe4bc76580
Jacob Patterson <jacob.patterson@gmail.com>
2020-06-15 16:49:28 -0700

    making request a struct

--- go/ui/user/request.go
+++ go/ui/user/request.go
@@ -3,6 +3,7 @@
 package user
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -12,19 +13,42 @@ import (
 	"github.com/jacobpatterson1549/selene-bananas/go/ui/log"
 )
 
+type (
+	request struct {
+		user      *User
+		form      dom.Form
+		validator func() bool
+		handler   func(body io.ReadCloser)
+	}
+)
+
 // Request makes a request to the server using the fields in the form.
 func (u User) request(f dom.Form) {
-	if f.URL.Path == "/user_delete" { // TODO: make responseHandler return a validation func AND response handler, for user_delete and user_create/modify additional username/password validations.
-		message := "Are you sure? All accumulated points will be lost"
-		if !dom.Confirm(message) {
+	r, err := u.newRequest(f)
+	switch {
+	case err != nil:
+		log.Error(err.Error())
+		return
+	case r.validator != nil:
+		if ok := r.validator(); !ok {
 			return
 		}
 	}
-	rh := u.responseHandler(f.URL.Path, f)
-	if rh == nil {
-		log.Error("Unknown action: " + f.URL.Path)
+	response, err := r.do(f)
+	switch {
+	case err != nil:
+		log.Error("making http request: " + err.Error())
 		return
+	case response.StatusCode >= 400:
+		log.Error(response.Status)
+		// TODO: logout user on http error?
+		return
+	case r.handler != nil:
+		r.handler(response.Body)
 	}
+}
+
+func (r request) do(f dom.Form) (*http.Response, error) {
 	var httpRequest *http.Request
 	var err error
 	// TODO: pass context when making http request
@@ -39,47 +63,41 @@ func (u User) request(f dom.Form) {
 		httpRequest, err = http.NewRequest(f.Method, url, body)
 		httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	default:
-		log.Error("unknown method: " + f.Method)
-		return
+		return nil, errors.New("unknown method: " + f.Method)
 	}
 	if err != nil {
-		log.Error("creating request: " + err.Error())
-		return
+		return nil, errors.New("creating request: " + err.Error())
 	}
 	if dom.GetChecked("has-login") {
-		jwt := u.JWT()
+		jwt := r.user.JWT()
 		httpRequest.Header.Set("Authorization", "Bearer "+jwt)
 	}
-	httpResponse, err := u.httpClient.Do(httpRequest)
-	switch {
-	case err != nil:
-		log.Error("making http request: " + err.Error())
-		return
-	case httpResponse.StatusCode >= 400:
-		log.Error(httpResponse.Status)
-		// TODO: logout user on http error?
-		return
-	default:
-		rh(httpResponse.Body)
-	}
+	return r.user.httpClient.Do(httpRequest)
 }
 
 // responseHandler creates a response-handling function for the url and form.  Nil is returned if the url is unknown.Path
-func (u *User) responseHandler(urlPath string, f dom.Form) func(body io.ReadCloser) {
-	switch urlPath {
+func (u *User) newRequest(f dom.Form) (*request, error) {
+	var validator func() bool
+	var handler func(body io.ReadCloser)
+	switch f.URL.Path {
 	case "/user_create":
-		return func(body io.ReadCloser) {
+		handler = func(body io.ReadCloser) {
 			username := f.Params.Get("username")
 			password := f.Params.Get("password")
 			dom.StoreCredentials(username, password)
 			u.Logout()
 		}
 	case "/user_delete":
-		return func(body io.ReadCloser) {
+		validator = func() bool {
+			message := "Are you sure? All accumulated points will be lost"
+			ok := dom.Confirm(message)
+			return ok
+		}
+		handler = func(body io.ReadCloser) {
 			u.Logout()
 		}
 	case "/user_login":
-		return func(body io.ReadCloser) {
+		handler = func(body io.ReadCloser) {
 			defer body.Close()
 			jwt, err := ioutil.ReadAll(body)
 			if err != nil {
@@ -92,17 +110,22 @@ func (u *User) responseHandler(urlPath string, f dom.Form) func(body io.ReadClos
 			u.login(string(jwt))
 		}
 	case "/user_update_password":
-		return func(body io.ReadCloser) {
+		handler = func(body io.ReadCloser) {
 			username := f.Params.Get("username")
 			password := f.Params.Get("password_confirm")
 			dom.StoreCredentials(username, password)
 			u.Logout()
 		}
 	case "/ping":
-		return func(body io.ReadCloser) {
-			// NOOP
-		}
+		// NOOP
 	default:
-		return nil
+		return nil, errors.New("Unknown action: " + f.URL.Path)
 	}
+	r := request{
+		user:      u,
+		form:      f,
+		validator: validator,
+		handler:   handler,
+	}
+	return &r, nil
 }
