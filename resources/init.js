8ca4122404fd2b1852ab5d510fba444f799f5262
Jacob Patterson <jacob.patterson@gmail.com>
2020-07-02 16:54:22 -0700

    renaming run_wasm.js to init.js

--- resources/init.js
+++ resources/init.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", () => {
+    // lod WebAssembly
     const go = new Go();
     WebAssembly.instantiateStreaming(fetch("/main.wasm?v={{.Version}}"), go.importObject)
         .then(async (result) => {
             await go.run(result.instance);
         });
-    // TODO: rename file to init.js
+    // register service worker
     // if ('serviceWorker' in navigator) {
     //     navigator.serviceWorker.register('/service-worker.js?v={{.Version}}');
     // }
