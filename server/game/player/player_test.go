7cdf66ecbef54760cb5655a1a915ff5934d5b89e
Jacob Patterson <jacob.patterson@gmail.com>
2020-08-06 12:44:03 -0700

    adding new player test

--- server/game/player/player_test.go
+++ server/game/player/player_test.go
@@ -1,6 +1,53 @@
 package player
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jacobpatterson1549/selene-bananas/game/board"
+)
+
+func TestNewPlayer(t *testing.T) {
+	newPlayerTests := []struct {
+		winPoints int
+		wantOk    bool
+	}{
+		{},
+		{
+			winPoints: 1,
+		},
+		{
+			winPoints: 2,
+			wantOk:    true,
+		},
+		{
+			winPoints: 10,
+			wantOk:    true,
+		},
+	}
+	for i, test := range newPlayerTests {
+		var b board.Board
+		cfg := Config{
+			WinPoints: test.winPoints,
+		}
+		p, err := cfg.New(&b)
+		switch {
+		case err != nil:
+			if test.wantOk {
+				t.Errorf("Test %v: unexpected error: %v", i, err)
+			}
+		case !test.wantOk:
+			t.Errorf("Test %v: expected error", i)
+		default:
+			if test.winPoints != p.winPoints {
+				t.Errorf("wanted %v winPoints, got %v", test.winPoints, p.winPoints)
+			}
+			b.NumCols = 22
+			if p.Board.NumCols != 22 {
+				t.Errorf("board reference not set correctly")
+			}
+		}
+	}
+}
 
 func TestDecrementWinPoints(t *testing.T) {
 	decrementWinPointsTests := []struct {
