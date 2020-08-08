f25948df4699d367b8b431e9863de28574689aeb
Jacob Patterson <jacob.patterson@gmail.com>
2020-08-08 11:34:18 -0700

    adding ui config
    This is similar to how the serever configures the main.  The code is now better split up, simplifying the actual main file.

---
+++ cmd/ui/config.go
@@ -0,0 +1,112 @@
+// +build js,wasm
+
+// Package main initializes interactive frontend elements and runs as long as the webpage is open.
+package main
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/jacobpatterson1549/selene-bananas/game/board"
+	"github.com/jacobpatterson1549/selene-bananas/ui/dom"
+	"github.com/jacobpatterson1549/selene-bananas/ui/game"
+	"github.com/jacobpatterson1549/selene-bananas/ui/game/canvas"
+	"github.com/jacobpatterson1549/selene-bananas/ui/game/lobby"
+	"github.com/jacobpatterson1549/selene-bananas/ui/game/socket"
+	"github.com/jacobpatterson1549/selene-bananas/ui/http/xhr"
+	"github.com/jacobpatterson1549/selene-bananas/ui/log"
+	"github.com/jacobpatterson1549/selene-bananas/ui/user"
+)
+
+// mainFlags contains options for the the ui.
+type mainFlags struct {
+	httpTimeout        time.Duration
+	tileLength         int
+	canvasDivQuery     string
+	canvasElementQuery string
+}
+
+// initDom creates, initializes, and links up dom components.
+func (m mainFlags) initDom(ctx context.Context, wg *sync.WaitGroup) {
+	log := m.log(ctx, wg)
+	user := m.user(ctx, wg, log)
+	board := new(board.Board)
+	canvas := m.canvas(ctx, wg, log, board)
+	game := m.game(ctx, wg, log, board, canvas)
+	lobby := m.lobby(ctx, wg, log, game)
+	socket := m.socket(ctx, wg, log, user, game, lobby)
+	user.Socket = socket   // [circular reference]
+	canvas.Socket = socket // [circular reference]
+	game.Socket = socket   // [circular reference]
+	lobby.Socket = socket  // [circular reference]
+}
+
+// log creates and initializes the log component.
+func (mainFlags) log(ctx context.Context, wg *sync.WaitGroup) *log.Log {
+	log := new(log.Log)
+	log.InitDom(ctx, wg)
+	return log
+}
+
+// user creates and initializes the user/form/http component.
+func (m mainFlags) user(ctx context.Context, wg *sync.WaitGroup, log *log.Log) *user.User {
+	cfg := user.Config{
+		Log: log,
+	}
+	httpClient := xhr.HTTPClient{
+		Timeout: m.httpTimeout,
+	}
+	user := cfg.New(httpClient)
+	user.InitDom(ctx, wg)
+	return user
+}
+
+// canvas creates and initializes the game drawing component with elements from the dom.
+func (m mainFlags) canvas(ctx context.Context, wg *sync.WaitGroup, log *log.Log, board *board.Board) *canvas.Canvas {
+	cfg := canvas.Config{
+		Log:        log,
+		TileLength: m.tileLength,
+	}
+	canvasDiv := dom.QuerySelector(m.canvasDivQuery)
+	canvasElement := dom.QuerySelector(m.canvasElementQuery)
+	canvas := cfg.New(board, &canvasDiv, &canvasElement)
+	canvas.InitDom(ctx, wg)
+	return canvas
+}
+
+// game creates and initializes the game component.
+func (mainFlags) game(ctx context.Context, wg *sync.WaitGroup, log *log.Log, board *board.Board, canvas *canvas.Canvas) *game.Game {
+	cfg := game.Config{
+		Log:    log,
+		Board:  board,
+		Canvas: canvas,
+	}
+	game := cfg.NewGame()
+	game.InitDom(ctx, wg)
+	return game
+}
+
+// lobby creates and initializes the game lobby component.
+func (mainFlags) lobby(ctx context.Context, wg *sync.WaitGroup, log *log.Log, game *game.Game) *lobby.Lobby {
+	cfg := lobby.Config{
+		Log:  log,
+		Game: game,
+	}
+	lobby := cfg.New()
+	lobby.InitDom(ctx, wg)
+	return lobby
+}
+
+// socket creates and initializes the player socket component for connection to the lobby.
+func (mainFlags) socket(ctx context.Context, wg *sync.WaitGroup, log *log.Log, user *user.User, game *game.Game, lobby *lobby.Lobby) *socket.Socket {
+	cfg := socket.Config{
+		Log:   log,
+		User:  user,
+		Game:  game,
+		Lobby: lobby,
+	}
+	socket := cfg.New()
+	socket.InitDom(ctx, wg)
+	return socket
+}
--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -9,110 +9,24 @@ import (
 	"syscall/js"
 	"time"
 
-	"github.com/jacobpatterson1549/selene-bananas/game/board"
 	"github.com/jacobpatterson1549/selene-bananas/ui/dom"
-	"github.com/jacobpatterson1549/selene-bananas/ui/game"
-	"github.com/jacobpatterson1549/selene-bananas/ui/game/canvas"
-	"github.com/jacobpatterson1549/selene-bananas/ui/game/lobby"
-	"github.com/jacobpatterson1549/selene-bananas/ui/game/socket"
-	"github.com/jacobpatterson1549/selene-bananas/ui/http/xhr"
-	"github.com/jacobpatterson1549/selene-bananas/ui/log"
-	"github.com/jacobpatterson1549/selene-bananas/ui/user"
 )
 
 // main initializes the wasm code for the web dom and runs as long as the browser is open.
 func main() {
+	m := mainFlags{
+		httpTimeout:        10 * time.Second,
+		tileLength:         20,
+		canvasDivQuery:     ".game>.canvas",
+		canvasElementQuery: ".game>.canvas>canvas",
+	}
 	ctx := context.Background()
-	var wg sync.WaitGroup
-	initDom(ctx, &wg)
-	wg.Wait() // BLOCKING
-}
-
-// initDom initializes and links up dom components.
-func initDom(ctx context.Context, wg *sync.WaitGroup) {
 	ctx, cancelFunc := context.WithCancel(ctx)
-	log := initLog(ctx, wg)
-	user := initUser(ctx, wg, log)
-	board := new(board.Board)
-	canvas := initCanvas(ctx, wg, log, board)
-	game := initGame(ctx, wg, log, board, canvas)
-	lobby := initLobby(ctx, wg, log, game)
-	socket := initSocket(ctx, wg, log, user, game, lobby)
-	user.Socket = socket   // [circular reference]
-	canvas.Socket = socket // [circular reference]
-	game.Socket = socket   // [circular reference]
-	lobby.Socket = socket  // [circular reference]
-	initBeforeUnloadFn(cancelFunc, wg)
+	var wg sync.WaitGroup
+	m.initDom(ctx, &wg)
 	enableInteraction()
-}
-
-// initLog initializes the log component.
-func initLog(ctx context.Context, wg *sync.WaitGroup) *log.Log {
-	log := new(log.Log)
-	log.InitDom(ctx, wg)
-	return log
-}
-
-// initLog initializes the user/form/http component.
-func initUser(ctx context.Context, wg *sync.WaitGroup, log *log.Log) *user.User {
-	cfg := user.Config{
-		Log: log,
-	}
-	xhrHTTPClient := xhr.HTTPClient{
-		Timeout: 10 * time.Second,
-	}
-	user := cfg.New(xhrHTTPClient)
-	user.InitDom(ctx, wg)
-	return user
-}
-
-// initLog initializes the game drawing component with elements from the dom.
-func initCanvas(ctx context.Context, wg *sync.WaitGroup, log *log.Log, board *board.Board) *canvas.Canvas {
-	cfg := canvas.Config{
-		Log:        log,
-		TileLength: 20,
-	}
-	canvasDiv := dom.QuerySelector(".game>.canvas")
-	canvasElement := dom.QuerySelector(".game>.canvas>canvas")
-	canvas := cfg.New(board, &canvasDiv, &canvasElement)
-	canvas.InitDom(ctx, wg)
-	return canvas
-}
-
-// initLog initializes the game component.
-func initGame(ctx context.Context, wg *sync.WaitGroup, log *log.Log, board *board.Board, canvas *canvas.Canvas) *game.Game {
-	cfg := game.Config{
-		Log:    log,
-		Board:  board,
-		Canvas: canvas,
-	}
-	game := cfg.NewGame()
-	game.InitDom(ctx, wg)
-	return game
-}
-
-// initLog initializes the game lobby component.
-func initLobby(ctx context.Context, wg *sync.WaitGroup, log *log.Log, game *game.Game) *lobby.Lobby {
-	cfg := lobby.Config{
-		Log:  log,
-		Game: game,
-	}
-	lobby := cfg.New()
-	lobby.InitDom(ctx, wg)
-	return lobby
-}
-
-// initLog initializes the player socket component for connection to the lobby.
-func initSocket(ctx context.Context, wg *sync.WaitGroup, log *log.Log, user *user.User, game *game.Game, lobby *lobby.Lobby) *socket.Socket {
-	cfg := socket.Config{
-		Log:   log,
-		User:  user,
-		Game:  game,
-		Lobby: lobby,
-	}
-	socket := cfg.New()
-	socket.InitDom(ctx, wg)
-	return socket
+	initBeforeUnloadFn(cancelFunc, &wg)
+	wg.Wait() // BLOCKING
 }
 
 // initBeforeUnloadFn registers a function to cancel the context when the browser is about to close.
@@ -135,8 +49,8 @@ func initBeforeUnloadFn(cancelFunc context.CancelFunc, wg *sync.WaitGroup) {
 // enableInteraction removes the disabled attribute from all submit buttons, allowing users to sign in and send other forms.
 func enableInteraction() {
 	document := dom.QuerySelector("body")
-	disabledSubmitButtons := dom.QuerySelectorAll(document, `input[type="submit"]:disabled`)
-	for _, disabledSubmitButton := range disabledSubmitButtons {
-		disabledSubmitButton.Set("disabled", false)
+	submitButtons := dom.QuerySelectorAll(document, `input[type="submit"]`)
+	for _, submitButton := range submitButtons {
+		submitButton.Set("disabled", false)
 	}
 }
