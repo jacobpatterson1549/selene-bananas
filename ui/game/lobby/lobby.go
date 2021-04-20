jacobpatterson1549/selene-bananas

commit 8f2c2ab9f1d11d4af85d076834f3cb8f4fd5ac2f

go language tweaks
removing int cast when comparing info status to Status.NotStarted
removing int cast in ui.lobby.SetGameInfos (gameID is an int)

ui/game/lobby/lobby.go
@@ -86,7 +86,7 @@ func (l *Lobby) SetGameInfos(gameInfos []game.Info, username string) {
 		status := gameInfo.Status.String()
 		rowElement.Get("children").Index(3).Set("innerHTML", status)
 		joinElements := rowElement.Get("children").Index(4)
-		joinElements.Get("children").Index(0).Set("value", int(gameInfo.ID))
+		joinElements.Get("children").Index(0).Set("value", gameInfo.ID)
 		if !gameInfo.CanJoin(username) {
 			joinElements.Get("children").Index(1).Call("setAttribute", "disabled", true)
 		}

game/info.go
@@ -31,7 +31,8 @@ type Info struct {
 // CanJoin indicates whether or not a player can join the game.
 // Players can only join games that are not started or that they were previously a part of, assuming there is room.
 func (i Info) CanJoin(playerName string) bool {
-	return i.isInGame(playerName) || (int(i.Status) == int(NotStarted) && len(i.Players) < i.Capacity)
+	return i.isInGame(playerName) ||
+		(i.Status == NotStarted && len(i.Players) < i.Capacity)
 }
 
 // isInGame determines if a player with the specified game is in the players slice.

commit 05eef773cb097d920ec334d43bf7662e30d6c4a8

REGRESSION FIX: ui lobby
fixed regression introduced in commit 8f2c2ab9f1d11d4af85d076834f3cb8f4fd5ac2f
 * The game.ID must be explicitly cast to an int before putin a js.Value.   breaking out gameInfoElement() func from SetGameInfos().

ui/game/lobby/lobby.go
@@ -75,21 +75,33 @@ func (l *Lobby) SetGameInfos(gameInfos []game.Info, username string) {
 		return
 	}
 	for _, gameInfo := range gameInfos {
-		gameInfoElement := dom.CloneElement(".game-info-row")
-		rowElement := gameInfoElement.Get("children").Index(0)
-		createdAtTimeText := dom.FormatTime(gameInfo.CreatedAt)
-		rowElement.Get("children").Index(0).Set("innerHTML", createdAtTimeText)
-		players := strings.Join(gameInfo.Players, ", ")
-		rowElement.Get("children").Index(1).Set("innerHTML", players)
-		capacityRatio := gameInfo.CapacityRatio()
-		rowElement.Get("children").Index(2).Set("innerHTML", capacityRatio)
-		status := gameInfo.Status.String()
-		rowElement.Get("children").Index(3).Set("innerHTML", status)
-		joinElements := rowElement.Get("children").Index(4)
-		joinElements.Get("children").Index(0).Set("value", gameInfo.ID)
-		if !gameInfo.CanJoin(username) {
-			joinElements.Get("children").Index(1).Call("setAttribute", "disabled", true)
-		}
+		gameInfoElement := gameInfoElement(gameInfo, username)
 		tbodyElement.Call("appendChild", gameInfoElement)
 	}
+}
+
+func gameInfoElement(gameInfo game.Info, username string) js.Value {
+	gameInfoElement := dom.CloneElement(".game-info-row")
+
+	rowElement := gameInfoElement.Get("children").Index(0)
+
+	createdAtTimeText := dom.FormatTime(gameInfo.CreatedAt)
+	rowElement.Get("children").Index(0).Set("innerHTML", createdAtTimeText)
+
+	players := strings.Join(gameInfo.Players, ", ")
+	rowElement.Get("children").Index(1).Set("innerHTML", players)
+
+	capacityRatio := gameInfo.CapacityRatio()
+	rowElement.Get("children").Index(2).Set("innerHTML", capacityRatio)
+
+	status := gameInfo.Status.String()
+	rowElement.Get("children").Index(3).Set("innerHTML", status)
+
+	joinElements := rowElement.Get("children").Index(4)
+	joinElements.Get("children").Index(0).Set("value", int(gameInfo.ID)) // must cast ID to int for js.Value conversion
+	if !gameInfo.CanJoin(username) {
+		joinElements.Get("children").Index(1).Call("setAttribute", "disabled", true)
+	}
+
+	return gameInfoElement
 }