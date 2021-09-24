d756e5e016b7bc49dcb7c662117ff90b1b25c6ae
Jacob Patterson <jacob.patterson@gmail.com>
2021-09-24 14:20:25 -0700

    adding more socket tests
    Making mock user and game structures in socket tests for ease of use.

--- ui/game/socket/mocks_test.go
+++ ui/game/socket/mocks_test.go
@@ -1,36 +1,48 @@
+//go:build js && wasm
+
 package socket
 
 import (
+	"syscall/js"
+
 	"github.com/jacobpatterson1549/selene-bananas/game"
 	"github.com/jacobpatterson1549/selene-bananas/game/message"
 )
 
-type mockUser string
+type mockUser struct {
+	JWTFunc      func() string
+	UsernameFunc func() string
+	LogoutFunc   func()
+}
 
 func (m mockUser) JWT() string {
-	return string(m)
+	return m.JWTFunc()
 }
 
 func (m mockUser) Username() string {
-	return string(m)
+	return m.UsernameFunc()
 }
 
-func (mockUser) Logout() {
-	// NOOP
+func (m mockUser) Logout() {
+	m.LogoutFunc()
 }
 
-type mockGame game.ID
+type mockGame struct {
+	IDFunc         func() game.ID
+	LeaveFunc      func()
+	UpdateInfoFunc func(msg message.Message)
+}
 
 func (m mockGame) ID() game.ID {
-	return game.ID(m)
+	return m.IDFunc()
 }
 
-func (mockGame) Leave() {
-	// NOOP
+func (m mockGame) Leave() {
+	m.LeaveFunc()
 }
 
-func (mockGame) UpdateInfo(m message.Message) {
-	// NOOP
+func (m *mockGame) UpdateInfo(msg message.Message) {
+	m.UpdateInfoFunc(msg)
 }
 
 type mockLobby struct {
@@ -39,4 +51,65 @@ type mockLobby struct {
 
 func (m mockLobby) SetGameInfos(gameInfos []game.Info, username string) {
 	m.SetGameInfosFunc(gameInfos, username)
+}
+
+type mockDOM struct {
+	QuerySelectorFunc    func(query string) js.Value
+	QuerySelectorAllFunc func(document js.Value, query string) []js.Value
+	SetCheckedFunc       func(query string, checked bool)
+	NewWebSocketFunc     func(url string) js.Value
+	NewJsFuncFunc        func(fn func()) js.Func
+	NewJsEventFuncFunc   func(fn func(event js.Value)) js.Func
+	AlertOnPanicFunc     func()
+}
+
+func (m mockDOM) QuerySelector(query string) js.Value {
+	return m.QuerySelectorFunc(query)
+}
+
+func (m mockDOM) QuerySelectorAll(document js.Value, query string) []js.Value {
+	return m.QuerySelectorAllFunc(document, query)
+}
+
+func (m *mockDOM) SetChecked(query string, checked bool) {
+	m.SetCheckedFunc(query, checked)
+}
+
+func (m *mockDOM) NewWebSocket(url string) js.Value {
+	return m.NewWebSocketFunc(url)
+}
+
+func (m *mockDOM) NewJsFunc(fn func()) js.Func {
+	return m.NewJsFuncFunc(fn)
+}
+
+func (m *mockDOM) NewJsEventFunc(fn func(event js.Value)) js.Func {
+	return m.NewJsEventFuncFunc(fn)
+}
+
+func (m *mockDOM) AlertOnPanic() {
+	m.AlertOnPanicFunc()
+}
+
+type mockLog struct {
+	InfoFunc    func(text string)
+	WarningFunc func(text string)
+	ErrorFunc   func(text string)
+	ChatFunc    func(text string)
+}
+
+func (m *mockLog) Info(text string) {
+	m.InfoFunc(text)
+}
+
+func (m *mockLog) Warning(text string) {
+	m.WarningFunc(text)
+}
+
+func (m *mockLog) Error(text string) {
+	m.ErrorFunc(text)
+}
+
+func (m *mockLog) Chat(text string) {
+	m.ChatFunc(text)
 }
--- ui/game/socket/socket_test.go
+++ ui/game/socket/socket_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"reflect"
 	"strconv"
+	"strings"
 	"syscall/js"
 	"testing"
 
@@ -50,9 +51,12 @@ func TestWebSocketURL(t *testing.T) {
 			URL:    *u,
 			Params: make(ui.Values, 1),
 		}
-		mu := mockUser(test.jwt)
 		s := Socket{
-			user: &mu,
+			user: &mockUser{
+				JWTFunc: func() string {
+					return test.jwt
+				},
+			},
 		}
 		got := s.webSocketURL(f)
 		if test.want != got {
@@ -141,31 +145,55 @@ func TestMessageJSON(t *testing.T) {
 }
 
 func TestSend(t *testing.T) {
-	webSocket := js.ValueOf(make(map[string]interface{}))
-	webSocket.Set("readyState", 1)
-	sendCalled := false
-	sendFunc := func(this js.Value, args []js.Value) interface{} {
-		want := `{"type":15,"info":"selene : hi","game":{"id":21}}`
-		got := args[0].String()
-		if want != got {
-			t.Errorf("not equal\nwanted %v\ngot    %v", want, got)
+	t.Run("not open", func(t *testing.T) {
+		errorLogged := false
+		s := Socket{
+			webSocket: js.Undefined(),
+			log: &mockLog{
+				ErrorFunc: func(text string) {
+					errorLogged = true
+				},
+			},
 		}
-		sendCalled = true
-		return nil
-	}
-	sendJsFunc := js.FuncOf(sendFunc)
-	defer sendJsFunc.Release()
-	webSocket.Set("send", sendJsFunc)
-	m := message.Message{Type: 15, Info: "selene : hi"}
-	g := mockGame(21)
-	s := Socket{
-		webSocket: webSocket,
-		game:      g,
-	}
-	s.Send(m)
-	if !sendCalled {
-		t.Errorf("send function not called")
-	}
+		m := message.Message{}
+		s.Send(m)
+		if !errorLogged {
+			t.Error("wanted error to be logged")
+		}
+	})
+	// t.Run("bad message json", func(t *testing.T) { }) // not tested
+	t.Run("happy path", func(t *testing.T) {
+		sendCalled := false
+		sendJsFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			want := `{"type":` + strconv.Itoa(int(message.GameChat)) + `,"info":"selene : hi","game":{"id":21}}`
+			got := args[0].String()
+			if want != got {
+				t.Errorf("not equal\nwanted %v\ngot    %v", want, got)
+			}
+			sendCalled = true
+			return nil
+		})
+		defer sendJsFunc.Release()
+		m := message.Message{
+			Type: message.GameChat,
+			Info: "selene : hi",
+		}
+		s := Socket{
+			webSocket: js.ValueOf(map[string]interface{}{
+				"readyState": 1,
+				"send":       sendJsFunc,
+			}),
+			game: &mockGame{
+				IDFunc: func() game.ID {
+					return 21
+				},
+			},
+		}
+		s.Send(m)
+		if !sendCalled {
+			t.Errorf("send function not called")
+		}
+	})
 }
 
 func TestOnMessage(t *testing.T) {
@@ -185,7 +213,11 @@ func TestOnMessage(t *testing.T) {
 				gameInfosSet = true
 			},
 		}
-		user := mockUser("fred")
+		user := &mockUser{
+			UsernameFunc: func() string {
+				return "fred"
+			},
+		}
 		s := Socket{
 			lobby: lobby,
 			user:  user,
@@ -195,4 +227,487 @@ func TestOnMessage(t *testing.T) {
 			t.Errorf("wanted game infos set")
 		}
 	})
+	t.Run("bad message json", func(t *testing.T) {
+		event := js.ValueOf(map[string]interface{}{
+			"data": `{bad json}`,
+		})
+		errorLogged := false
+		s := Socket{
+			log: &mockLog{
+				ErrorFunc: func(text string) {
+					errorLogged = true
+				},
+			},
+		}
+		s.onMessage(event)
+		if !errorLogged {
+			t.Error("wanted error to be logged")
+		}
+	})
+	t.Run("logging", func(t *testing.T) {
+		tests := []struct {
+			messageType message.Type
+			want        int
+		}{
+			{
+				messageType: -1, // bad type => error
+				want:        1,
+			},
+			{
+				messageType: message.SocketError,
+				want:        1,
+			},
+			{
+				messageType: message.SocketWarning,
+				want:        2,
+			},
+			{
+				messageType: message.GameChat,
+				want:        3,
+			},
+		}
+		for i, test := range tests {
+			event := js.ValueOf(map[string]interface{}{
+				"data": `{"type":` + strconv.Itoa(int(test.messageType)) + `}`,
+			})
+			got := 0
+			s := Socket{
+				log: &mockLog{
+					ErrorFunc: func(text string) {
+						got = 1
+					},
+					WarningFunc: func(text string) {
+						got = 2
+					},
+					ChatFunc: func(text string) {
+						got = 3
+					},
+				},
+			}
+			s.onMessage(event)
+			if test.want != got {
+				t.Errorf("Test %v: wanted log type %v to be called, got %v", i, test.want, got)
+			}
+		}
+	})
+	t.Run("handlers", func(t *testing.T) {
+		tests := []struct {
+			messageType     message.Type
+			wantActionType  int
+			wantSocketClose bool
+		}{
+			{
+				messageType:     message.PlayerRemove,
+				wantActionType:  1,
+				wantSocketClose: true,
+			},
+			{
+				messageType:    message.LeaveGame,
+				wantActionType: 1,
+			},
+			{
+				messageType:    message.JoinGame,
+				wantActionType: 2,
+			},
+			{
+				messageType:    message.ChangeGameStatus,
+				wantActionType: 2,
+			},
+			{
+				messageType:    message.ChangeGameTiles,
+				wantActionType: 2,
+			},
+			{
+				messageType:    message.RefreshGameBoard,
+				wantActionType: 2,
+			},
+		}
+		for i, test := range tests {
+			event := js.ValueOf(map[string]interface{}{
+				"data": `{"info":"any","type":` + strconv.Itoa(int(test.messageType)) + `}`,
+			})
+			infoLogged := false
+			socketClosed := false
+			gotAction := 0
+			closeWebSocket := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				socketClosed = true
+				return nil
+			})
+			s := Socket{
+				webSocket: js.ValueOf(map[string]interface{}{
+					"readyState": 1,
+					"close":      closeWebSocket,
+				}),
+				log: &mockLog{
+					InfoFunc: func(text string) {
+						infoLogged = true
+					},
+				},
+				game: &mockGame{
+					LeaveFunc: func() {
+						gotAction = 1
+					},
+					UpdateInfoFunc: func(msg message.Message) {
+						gotAction = 2
+					},
+				},
+				dom: &mockDOM{
+					SetCheckedFunc: func(query string, checked bool) {
+						if !test.wantSocketClose {
+							t.Errorf("Test %v: dom should only be used when removing player", i)
+						}
+					},
+				},
+			}
+			s.onMessage(event)
+			closeWebSocket.Release()
+			if !infoLogged {
+				t.Error("wanted info to be logged")
+			}
+			if want, got := test.wantActionType, gotAction; want != got {
+				t.Errorf("Test %v: action types not equal: wanted %v, got %v", i, want, got)
+			}
+			if want, got := test.wantSocketClose, socketClosed; want != got {
+				t.Errorf("Test %v: socket closed calls not equal: wanted %v, got %v", i, want, got)
+			}
+		}
+		t.Run("httpPing", func(t *testing.T) {
+			event := js.ValueOf(map[string]interface{}{
+				"data": `{"type":` + strconv.Itoa(int(message.SocketHTTPPing)) + `}`,
+			})
+			pingHandled := false
+			requestSubmit := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				pingHandled = true
+				return nil
+			})
+			s := Socket{
+				dom: &mockDOM{
+					QuerySelectorFunc: func(query string) js.Value {
+						return js.ValueOf(map[string]interface{}{
+							"requestSubmit": requestSubmit,
+						})
+					},
+				},
+			}
+			s.onMessage(event)
+			requestSubmit.Release()
+			if !pingHandled {
+				t.Error("wanted ping to be handled")
+			}
+		})
+	})
+}
+
+func TestNew(t *testing.T) {
+	dom := &mockDOM{}
+	log := &mockLog{}
+	user := &mockUser{}
+	game := &mockGame{}
+	lobby := &mockLobby{}
+	want := &Socket{
+		dom:   dom,
+		log:   log,
+		user:  user,
+		game:  game,
+		lobby: lobby,
+	}
+	got := New(dom, log, user, game, lobby)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("sockets not equal\nwatned: %#v\ngot:    %#v", want, got)
+	}
+}
+
+func TestConnect(t *testing.T) {
+	tests := []struct {
+		webSocket js.Value
+		event     js.Value
+		wantOk    bool
+	}{
+		{ // already open
+			webSocket: js.ValueOf(map[string]interface{}{
+				"readyState": 1,
+			}),
+			wantOk: true,
+		},
+		{ // bad form
+			webSocket: js.ValueOf(map[string]interface{}{
+				"readyState": 0,
+			}),
+			event: js.ValueOf(map[string]interface{}{
+				"target": map[string]interface{}{
+					"method": "get",
+					"action": "bad_url",
+				},
+			}),
+		},
+		// happy path testing may be a challenge because Connect call blocks
+	}
+	for i, test := range tests {
+		s := Socket{
+			webSocket: test.webSocket,
+			dom: &mockDOM{
+				QuerySelectorAllFunc: func(document js.Value, query string) (all []js.Value) {
+					return
+				},
+			},
+			user: mockUser{
+				JWTFunc: func() string {
+					return "user.jwt.token"
+				},
+			},
+		}
+		err := s.Connect(test.event)
+		if want, got := test.wantOk, err == nil; want != got {
+			t.Errorf("Test %v: connect errors not equal: wanted %v, got %v (%v)", i, want, got, err)
+		}
+	}
+}
+
+func TestOnOpen(t *testing.T) {
+	setCheckedCalled := false
+	s := Socket{
+		dom: &mockDOM{
+			SetCheckedFunc: func(query string, checked bool) {
+				setCheckedCalled = true
+			},
+		},
+	}
+	errC := make(chan error, 1)
+	f := s.onOpen(errC)
+	f()
+	if got := <-errC; got != nil {
+		t.Errorf("wanted nil error sent on error channel when socket is opened, got %v", got)
+	}
+	if !setCheckedCalled {
+		t.Error("wanted dom to be modified when socket opened via SetChecked")
+	}
+}
+
+func TestOnClose(t *testing.T) {
+	tests := []struct {
+		event       js.Value
+		wantWarning bool
+	}{
+		{
+			event: js.ValueOf(map[string]interface{}{}),
+		},
+		{
+			event: js.ValueOf(map[string]interface{}{"reason": ""}),
+		},
+		{
+			event:       js.ValueOf(map[string]interface{}{"reason": "any"}),
+			wantWarning: true,
+		},
+	}
+	for i, test := range tests {
+		warningLogged := false
+		setCheckedCalled := false
+		s := Socket{
+			webSocket: js.ValueOf(map[string]interface{}{}),
+			log: &mockLog{
+				WarningFunc: func(text string) {
+					warningLogged = true
+				},
+			},
+			dom: &mockDOM{
+				SetCheckedFunc: func(query string, checked bool) {
+					setCheckedCalled = true
+				},
+			},
+		}
+		s.onClose(test.event)
+		if want, got := test.wantWarning, warningLogged; want != got {
+			t.Errorf("Test %v: wantWarning not equal: wanted %v, got %v", i, want, got)
+		}
+		if !setCheckedCalled {
+			t.Error("wanted something to be checked to indicate that the socket is closed")
+		}
+	}
+}
+
+func TestOnError(t *testing.T) {
+	userLoggedOut := false
+	s := Socket{
+		user: mockUser{
+			LogoutFunc: func() {
+				userLoggedOut = true
+			},
+		},
+	}
+	errC := make(chan error, 1)
+	f := s.onError(errC)
+	f()
+	if got := <-errC; got == nil {
+		t.Error("wanted non-nil error to be reported when socket receives an error")
+	}
+	if !userLoggedOut {
+		t.Error("wanted user to be logged out")
+	}
+}
+
+func TestCloseWebSocket(t *testing.T) {
+	funcNames := []string{"onopen", "onclose", "onerror", "onmessage"}
+	webSocket := js.ValueOf(map[string]interface{}{
+		"onopen":    true,
+		"onclose":   true,
+		"onerror":   true,
+		"onmessage": true,
+	})
+	s := Socket{
+		webSocket: webSocket,
+		dom: &mockDOM{
+			SetCheckedFunc: func(query string, checked bool) {
+				// NOOP
+			},
+		},
+	}
+	s.closeWebSocket()
+	for _, funcName := range funcNames {
+		if want, got := js.Null(), webSocket.Get(funcName); !want.Equal(got) {
+			t.Errorf("wanted %v func on webSocket to be null, got %v", funcName, got)
+		}
+	}
+}
+
+// TestClose checks the closing behavior for different isOpen states
+func TestClose(t *testing.T) {
+	var closeCalled bool
+	close := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		closeCalled = true
+		return nil
+	})
+	tests := []struct {
+		webSocket          js.Value
+		wantCloseWebSocket bool
+	}{
+		{},
+		{
+			webSocket: js.ValueOf(map[string]interface{}{
+				"readyState": 1,
+				"close":      close,
+			}),
+			wantCloseWebSocket: true,
+		},
+	}
+	for i, test := range tests {
+		closeCalled = false
+		leaveCalled := false
+		s := Socket{
+			webSocket: test.webSocket,
+			game: &mockGame{
+				LeaveFunc: func() {
+					leaveCalled = true
+				},
+			},
+			dom: &mockDOM{
+				SetCheckedFunc: func(query string, checked bool) {
+					// NOOP
+				},
+			},
+		}
+		s.Close()
+		if !leaveCalled {
+			t.Errorf("Test %v: wanted game to be left", i)
+		}
+		if want, got := test.wantCloseWebSocket, closeCalled; want != got {
+			t.Errorf("Test %v: wanted web socket to be closed: %v, got %v", i, want, got)
+		}
+	}
+	close.Release()
+}
+
+// TestHandle tests handleGameLeave, handlePlayerRemove and handleInfo
+func TestHandle(t *testing.T) {
+	socketTests := []struct {
+		name       string
+		handle     func(s Socket, m message.Message)
+		wantCallID int
+	}{
+		{
+			name:       "TestHandleGameLeave",
+			handle:     func(s Socket, m message.Message) { s.handleGameLeave(m) },
+			wantCallID: 1,
+		},
+		{
+			name:       "TestHandlePlayerRemove",
+			handle:     func(s Socket, m message.Message) { s.handlePlayerRemove(m) },
+			wantCallID: 1,
+		},
+		{
+			name:       "TestHandleInfo",
+			handle:     func(s Socket, m message.Message) { s.handleInfo(m) },
+			wantCallID: 2,
+		},
+	}
+	for _, subTest := range socketTests {
+		t.Run(subTest.name, func(t *testing.T) {
+			handleTests := []struct {
+				info    string
+				wantLog bool
+			}{
+				{},
+				{
+					info:    "stuff to log",
+					wantLog: true,
+				},
+			}
+			for i, test := range handleTests {
+				callID := 0
+				infoLogged := false
+				m := message.Message{
+					Info: test.info,
+				}
+				s := Socket{
+					game: &mockGame{
+						LeaveFunc: func() {
+							callID = 1
+						},
+						UpdateInfoFunc: func(msg message.Message) {
+							callID = 2
+						},
+					},
+					log: &mockLog{
+						InfoFunc: func(text string) {
+							if want, got := test.info, text; want != got {
+								t.Errorf("Test %v: logged info not equal: wanted %v, got %v", i, want, got)
+							}
+							infoLogged = true
+						},
+					},
+				}
+				subTest.handle(s, m)
+				if want, got := subTest.wantCallID, callID; want != got {
+					t.Errorf("Test %v: wanted call %v to be made, got %v", i, want, got)
+				}
+				if want, got := test.wantLog, infoLogged; want != got {
+					t.Errorf("Test %v: log.info() call not equal: wanted %v, got %v", i, want, got)
+				}
+			}
+		})
+	}
+}
+
+func TestHttpPing(t *testing.T) {
+	triggered := false
+	requestSubmit := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		triggered = true
+		return nil
+	})
+	s := Socket{
+		dom: &mockDOM{
+			QuerySelectorFunc: func(query string) js.Value {
+				if want, got := "ping", query; !strings.Contains(got, want) {
+					t.Errorf("queried ping form element did not contain %q: %q", want, got)
+				}
+				return js.ValueOf(map[string]interface{}{
+					"requestSubmit": requestSubmit,
+				})
+			},
+		},
+	}
+	s.httpPing()
+	requestSubmit.Release()
+	if !triggered {
+		t.Error("request submit not triggered")
+	}
 }
