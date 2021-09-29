e4d7999d6a91b6d67d6c0fce174ad811c11f308e
Jacob Patterson <jacob.patterson@gmail.com>
2021-09-29 06:59:26 -0700

    removing ui.http::mockXMLHttpRequest struct
    the tests are done without using a test-specific structure

--- ui/http/xhr_test.go
+++ ui/http/xhr_test.go
@@ -3,6 +3,7 @@
 package http
 
 import (
+	"reflect"
 	"syscall/js"
 	"testing"
 	"time"
@@ -20,10 +21,16 @@ func TestDoRequest(t *testing.T) {
 	tests := []struct {
 		Client
 		Request
-		mockXMLHttpRequest
-		eventType string
-		want      Response
-		wantOk    bool
+		eventType          string
+		want               Response
+		wantOk             bool
+		responseStatus     int
+		responseBody       string
+		wantOpenMethod     string
+		wantOpenURL        string
+		wantTimeout        int
+		wantRequestHeaders map[string]string
+		wantBody           string
 	}{
 		{
 			eventType: "timeout",
@@ -35,6 +42,12 @@ func TestDoRequest(t *testing.T) {
 			eventType: "error",
 		},
 		{
+			eventType: "load",
+			want: Response{
+				Code: 200,
+				Body: "successful body 16",
+			},
+			wantOk: true,
 			Client: Client{
 				Timeout: 15 * time.Second,
 			},
@@ -47,31 +60,60 @@ func TestDoRequest(t *testing.T) {
 				},
 				Body: "[[request body]]",
 			},
-			mockXMLHttpRequest: mockXMLHttpRequest{
-				status:            200,
-				response:          "successful body 16",
-				wantOpenMethod:    "GET",
-				wantOpenURL:       "https://example.com",
-				gotRequestHeaders: make(map[string]string, 2),
-				wantRequestHeaders: map[string]string{
-					"Content-Type":  "text/plain",
-					"Authorization": "Bearer s3cr3t",
-				},
-				wantBody: "[[request body]]",
+			responseStatus: 200,
+			responseBody:   "successful body 16",
+			wantOpenMethod: "GET",
+			wantOpenURL:    "https://example.com",
+			wantRequestHeaders: map[string]string{
+				"Content-Type":  "text/plain",
+				"Authorization": "Bearer s3cr3t",
 			},
-			eventType: "load",
-			want: Response{
-				Code: 200,
-				Body: "successful body 16",
-			},
-			wantOk: true,
+			wantBody: "[[request body]]",
 		},
 	}
 	for i, test := range tests {
 		var jsFuncs []js.Func
+		gotOpenMethod := ""
+		gotTimeout := 0
+		gotOpenURL := ""
+		gotRequestHeaders := make(map[string]string)
+		gotBody := ""
 		dom := mockDOM{
 			NewXHRFunc: func() js.Value {
-				return test.mockXMLHttpRequest.jsValue(test.eventType)
+				eventListeners := make(map[string]js.Value, 4)
+				open := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					gotOpenMethod = args[0].String()
+					gotOpenURL = args[1].String()
+					return nil
+				})
+				setRequestHeader := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					k, v := args[0].String(), args[1].String()
+					gotRequestHeaders[k] = v
+					return nil
+				})
+				addEventListener := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					eventType, handler := args[0].String(), args[1]
+					eventListeners[eventType] = handler
+					return nil
+				})
+				send := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					gotBody = args[0].String()
+					handler := eventListeners[test.eventType]
+					event := js.ValueOf(map[string]interface{}{
+						"type": test.eventType,
+					})
+					handler.Invoke(event)
+					return nil
+				})
+				jsFuncs = append(jsFuncs, open, setRequestHeader, addEventListener, send)
+				return js.ValueOf(map[string]interface{}{
+					"open":             open,
+					"setRequestHeader": setRequestHeader,
+					"addEventListener": addEventListener,
+					"send":             send,
+					"status":           test.responseStatus,
+					"response":         test.responseBody,
+				})
 			},
 			NewJsEventFuncFunc: func(fn func(event js.Value)) js.Func {
 				f := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
@@ -87,13 +129,21 @@ func TestDoRequest(t *testing.T) {
 		for _, f := range jsFuncs {
 			f.Release()
 		}
-		test.mockXMLHttpRequest.Release()
-		test.mockXMLHttpRequest.checkCalls(t)
 		switch {
+		case test.wantOpenMethod != gotOpenMethod:
+			t.Errorf("xhr open methods not equal:\nwanted: %v\ngot:    %v", test.wantOpenMethod, gotOpenMethod)
+		case test.wantTimeout != gotTimeout:
+			t.Errorf("xhr timeouts equal:\nwanted: %v\ngot:    %v", test.wantTimeout, gotTimeout)
+		case test.wantOpenURL != gotOpenURL:
+			t.Errorf("xhr open urls not equal:\nwanted: %v\ngot:    %v", test.wantOpenURL, gotOpenURL)
+		case test.wantBody != gotBody:
+			t.Errorf("xhr bodies not equal:\nwanted: %v\ngot:    %v", test.wantBody, gotBody)
 		case !test.wantOk:
 			if err == nil {
 				t.Errorf("Test %v: wanted error when eventType is %v, got %#v", i, test.eventType, *got)
 			}
+		case !reflect.DeepEqual(test.wantRequestHeaders, gotRequestHeaders): // checking after wantOk because wantRequestHeaders is often nil
+			t.Errorf("xhr open args not equal:\nwanted: %v\ngot:    %v", test.wantRequestHeaders, gotRequestHeaders)
 		case err != nil:
 			t.Errorf("Test %v: unwanted error: %v", i, err)
 		case test.want != *got:
